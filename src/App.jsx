00cfa060442bfd2e3b4cb7c218ee3c5f954c122f
ChristianScarabelli <scachmabol@hotmail.it>
2024-12-12 11:38:45 +0100

    ADD: repo with axios,bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>ciao</h1>
+    </>
+  )
+}
+
+export default App
